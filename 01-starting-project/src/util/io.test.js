f9200de672cf964bf4c1747701e46bcb25fc7540
Hui Beom Kim <khuibeom@gmail.com>
2023-04-06 14:16:21 +0900

    feat: added tests for spies and mocking

---
+++ 01-starting-project/src/util/io.test.js
@@ -0,0 +1,35 @@
+import { it, expect, vi } from 'vitest'
+import { promises as fs } from 'fs'
+
+import writeData from './io';
+
+vi.mock('fs')
+vi.mock('path', () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1]
+            }
+        }
+    }
+})
+
+it('should execute the writeFile method', () => {
+    const testData = 'Test';
+    const testFilename = 'test.txt';
+
+    writeData(testData, testFilename);
+
+    expect(fs.writeFile).toBeCalledWith(testFilename, testData)
+    // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+})
+
+it('should return a promise that resolves to no value if called correctly', () => {
+    const testData = 'Test';
+    const testFilename = 'test.txt';
+
+    writeData(testData, testFilename);
+
+    // expect(fs.writeFile).toBeCalledWith(testFilename, testData)
+    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+})
