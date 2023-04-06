19cbc5b157a91ed9e0172f45abe06ff8358449df
Hui Beom Kim <khuibeom@gmail.com>
2023-04-06 11:38:24 +0900

    feat: added integration test

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,19 +1,39 @@
-import { it, expect } from "vitest";
-import { transformToNumber } from "./numbers";
+import { it, expect, describe } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it('should transform a string number to a number of type number', () => {
-    const input = '1';
-    const result = transformToNumber(input);
-    expect(result).toBeTypeOf('number')
+describe('transformToNumber()', () => {
+    it('should transform a string number to a number of type number', () => {
+        const input = '1';
+        const result = transformToNumber(input);
+        expect(result).toBeTypeOf('number')
+    })
+    
+    it('should yield NaN for non-transformable values', () => {
+        const input = 'invalid';
+        const input2 = {};
+    
+        const result = transformToNumber(input);
+        const result2 = transformToNumber(input2);
+    
+        expect(result).toBeNaN()
+        expect(result2).toBeNaN()
+    })
 })
 
-it('should yield NaN for non-transformable values', () => {
-    const input = 'invalid';
-    const input2 = {};
+describe('cleanNumbers()', () => {
+    it('should return an array of number values if an array of string number values is provided', () => {
+        const numberValues = ['1', '2']
 
-    const result = transformToNumber(input);
-    const result2 = transformToNumber(input2);
+        const cleanedNumbers = cleanNumbers(numberValues);
+        expect(cleanedNumbers[0]).toBeTypeOf('number')
+    })
 
-    expect(result).toBeNaN()
-    expect(result2).toBeNaN()
+    it('should should throw an error if an array of at least one empty string is provided', () => {
+        const numberValues = ['', '2']
+
+        const resultFn = () => {
+            cleanNumbers(numberValues);
+        }
+        expect(resultFn).toThrow()
+    })
 })
